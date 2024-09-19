f83aaa32aadf96aa18b90e9cccc63d335f6dfd3e
fato52 <126178928+fato52@users.noreply.github.com>
2024-09-19 12:55:01 +0300

    r2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -6,6 +6,7 @@ public class Runner {
         System.out.println("versiyon 2nin devami");
         System.out.println("versiyon 3");
         System.out.println("versiyon 4");
+        //remote degisiklik
         //hgcvhjbjkln
         //jkhvkbl
     }
